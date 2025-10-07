4383655ab8e9f0487ee5f71be2aded6f275b9df7
Elies75013 <elies.benyahia@laplateforme.io>
2025-10-07 12:44:34 +0200

    Update index.php

--- jour02/job07/index.php
+++ jour02/job07/index.php
@@ -1,68 +1,28 @@
+<!doctype html>
+<html lang="fr"> 
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Job 07</title>
+</head> 
+
+<body>
+   <style>
+    div { 
+        text-align: center; 
+    }
+   </style> 
 <?php
-function gras($str) {
-    $mots = explode(" ", $str);
-    foreach ($mots as &$mot) {
-        if (ctype_upper(substr($mot, 0, 1))) {
-            $mot = "<b>$mot</b>";
-        }
+   $hauteur = 5;
+   echo "<div>";
+    for ($i = 1; $i <= $hauteur; $i++) {
+         for ($j = 1; $j <= $i; $j++) {
+              echo " *";
+         }
+         echo "<br />";
     }
-    return implode(" ", $mots);
-}
-
-function cesar($str, $decalage = 2) {
-    $res = "";
-    for ($i = 0; $i < strlen($str); $i++) {
-        $c = $str[$i];
-        if (ctype_alpha($c)) {
-            $base = ctype_upper($c) ? 'A' : 'a';
-            $res .= chr((ord($c) - ord($base) + $decalage) % 26 + ord($base));
-        } else {
-            $res .= $c;
-        }
-    }
-    return $res;
-}
-
-function plateforme($str) {
-    $mots = explode(" ", $str);
-    foreach ($mots as &$mot) {
-        if (substr($mot, -2) == "me") {
-            $mot .= "_";
-        }
-    }
-    return implode(" ", $mots);
-}
-
-$resultat = "";
-
-if (isset($_POST["str"]) && isset($_POST["fonction"])) {
-    $str = $_POST["str"];
-    $fonction = $_POST["fonction"];
-
-    if ($fonction == "gras") {
-        $resultat = gras($str);
-    } elseif ($fonction == "cesar") {
-        $resultat = cesar($str);
-    } elseif ($fonction == "plateforme") {
-        $resultat = plateforme($str);
-    }
-}
+    echo "</div>"; 
 ?>
-
-
-<form method="POST">
-    <input type="text" name="str" placeholder="Entrez du texte" required>
-    <select name="fonction">
-        <option value="gras">Gras</option>
-        <option value="cesar">César</option>
-        <option value="plateforme">Plateforme</option>
-    </select>
-    <button type="submit">Valider</button>
-</form>
-
-
-<?php
-if (!empty($resultat)) {
-    echo "<p><strong>Résultat :</strong> $resultat</p>";
-}
-?>
+</body>
+</html>
